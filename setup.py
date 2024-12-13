84ee6adeff240f58d59ae2ac577e4370cc9e99a6
Sami <sami.leguil@gmail.com>
2024-12-13 16:24:47 +0100

    Update setup.py v 0.2.0
    v 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Correct_TDS',
-    version='0.1.20',    
+    version='0.2.0',    
     description='Correct TDS software for statistical gap analysis',
     url='https://github.com/THzbiophotonics/Correct-TDS-in-development/tree/master',
     author='PERETTI Romain',
