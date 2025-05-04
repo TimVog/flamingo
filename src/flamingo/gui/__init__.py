38d7bc2170a50c7032fac6a9f9506cdd8d9cdca0
Tim <32876663+TimVog@users.noreply.github.com>
2025-05-04 15:50:12 +0200

    Improved entrypoint for GUI (name conflict between original gui.py and module called gui), now it just app.py in the module "gui"

--- src/flamingo/gui/__init__.py
+++ src/flamingo/gui/__init__.py
@@ -1,1 +1,6 @@
+"""GUI components for the Flamingo application."""
+
+# Import components 
 from flamingo.gui.components import ControlPanel, VisualizationPanel
+# Import the main function
+from .app import main
